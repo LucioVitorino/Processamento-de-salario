8974703ecc43b4462e905c252d8b3c0832417457
Jofre Jaime <jofredenovais@gmail.com>
2025-05-22 01:57:38 +0100

    main alterado

--- src/isptec/pii_pt2/grupo1/Processamento_de_salario.java
+++ src/isptec/pii_pt2/grupo1/Processamento_de_salario.java
@@ -15,7 +15,14 @@ public class Processamento_de_salario {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Collaborator colab = new Collaborator();
+        colab.create_collaborator();
+        
+        System.out.println("Nome: " + colab.name);
+        System.out.println("Data de nascimento: " + colab.birthday);
+        System.out.println("Email: " + colab.email);
+        System.out.println("Função: " + colab.function.name);
+        System.out.println("Endereço: " + colab.household.street);
     }
     
 }
