53200ddf5a39733b7babca415e66f0b80cec0fd7
Jofre Jaime <jofredenovais@gmail.com>
2025-05-24 05:40:47 +0100

    feito.

--- src/isptec/pii_pt2/grupo1/Menu.java
+++ src/isptec/pii_pt2/grupo1/Menu.java
@@ -51,7 +51,7 @@ public class Menu {
                 System.out.println();
                 break;
             case 5:
-                list_collaboratores(list);
+                list_collaborators(list);
                 break;
             case 0:
                 System.out.println("Voltando ao Menu Principal...");
